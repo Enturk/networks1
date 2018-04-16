6184027da10d6b69d37cf35bf17d436431f7519b
tim039 <ttim@udel.edu>
2018-04-15 20:45:18 -0400

    Update tcpclient.c

--- tcpclient.c
+++ tcpclient.c
@@ -1,6 +1,7 @@
-/* tcp_ client.c */ 
-/* Programmed by Adarsh Sethi & Nazim Karaca*/
-/* February 21, 2018 & April 2, 2018*/     
+/*
+/* tcp_ client.c */
+/* Programmed by Adarsh Sethi, Nazim Karaca, & Timothy Louie*/
+/* February 21, 2018 & April 2, 2018*/
 
 #include <stdio.h>          /* for standard I/O functions */
 #include <stdlib.h>         /* for exit */
@@ -26,11 +27,11 @@ int main(void) {
 
    char sentence[STRING_SIZE];  /* send message */
    char receivedSentence[STRING_SIZE]; /* receive message */
-   unsigned int msg_len;  /* length of message */                      
+   unsigned int msg_len;  /* length of message */
    int bytes_sent, bytes_recd; /* number of bytes sent or received */
- 
+
    char packetRecd[STRING_SIZE];	// recieved packet
- 
+
    short debug = 0; // TODO change to 0 when done
    struct packetHeader { // to use with pointers when receiving headers
       int sequenceNumber;
@@ -40,7 +41,7 @@ int main(void) {
    int i; // for the for loop
 
    int dataSize = 0; // for end-of-transmission report
-   int packetQty = 0; 
+   int packetQty = 0;
 
    time_t rawtime; // for timestamp
    struct tm * timeinfo;
@@ -53,7 +54,7 @@ int main(void) {
       exit(1);
    }
 
-   /* Note: there is no need to initialize local client address information 
+   /* Note: there is no need to initialize local client address information
             unless you want to specify a specific local port
             (in which case, do it the same way as in udpclient.c).
             The local address initialization and binding is done automatically
@@ -81,14 +82,14 @@ int main(void) {
    server_addr.sin_port = htons(server_port);
 
     /* connect to the server */
- 		
-   if (connect(sock_client, (struct sockaddr *) &server_addr, 
+
+   if (connect(sock_client, (struct sockaddr *) &server_addr,
                                     sizeof (server_addr)) < 0) {
       perror("Client: can't connect to server");
       close(sock_client);
       exit(1);
    }
-  
+
    /* user interface */
 
    printf("Please type a filename (including the extension such as .txt or .c):\n");
@@ -96,10 +97,10 @@ int main(void) {
    msg_len = strlen(sentence) + 1;
 
    // prep header
-   struct packetHeader fileRequestHeader; 
+   struct packetHeader fileRequestHeader;
    fileRequestHeader.sequenceNumber = 0;
    fileRequestHeader.count = msg_len;
-   
+
    /* send header */
    bytes_sent = send(sock_client, &fileRequestHeader, sizeof(fileRequestHeader), 0); // Sethi trick
 
@@ -147,7 +148,7 @@ int main(void) {
    if(pFile==NULL) {
       perror("Error opening file.");
       close(sock_client);
-      exit(1); 
+      exit(1);
    }
    if (debug == 1) {
       timeinfo = localtime(&rawtime);
@@ -159,11 +160,15 @@ int main(void) {
    while (catchFileHeader.sequenceNumber != 0){
 //FIXME none of this loop seems to be running but it does loop (packetQty increments)
      if (debug == 1) printf("In file reception while loop\n");
-      /* get header response from server */  
-      bytes_recd = recv(sock_client, &catchFileHeader, sizeof(catchFileHeader), 0); 
+      /* get header response from server */
+      bytes_recd = recv(sock_client, &catchFileHeader, sizeof(catchFileHeader), 0);
       catchFileHeader.count = ntohs(catchFileHeader.count);
       catchFileHeader.sequenceNumber = ntohs(catchFileHeader.sequenceNumber);
       msg_len=catchFileHeader.count;
+
+      printf("packet %d is received with ", catchFileHeader.sequenceNumber );
+      printf("%d bytes\n", catchFileHeader.count);
+
 
 	// error checking
       if (bytes_recd < 0) {
@@ -181,7 +186,7 @@ int main(void) {
             printf("Received header packet number field is: %d\n", catchFileHeader.sequenceNumber);
             printf("Received header data count field is: %d\n\n", msg_len);
          }
-      }         
+      }
 
 	// receive data from server
       bytes_recd = recv(sock_client, receivedSentence, msg_len, 0);
@@ -217,10 +222,18 @@ int main(void) {
       if (debug == 1)
          printf("Appended this line to file:\n%s\n", receivedSentence);
 
+      if(catchFileHeader.sequenceNumber == 0) {
+    	  printf("End of Transmission Packet with sequence number %d ", packetQty);
+    	  printf("received with %d data bytes", msg_len);
+    	  packetQty--;                                      //while loop catches last packet
+      }
+
 	// final report tracking
       packetQty++;
       dataSize+=msg_len;
    }
+
+
 
    // final report
    printf("Number of data packets received: %d\n", packetQty);
@@ -230,4 +243,8 @@ int main(void) {
    /* close the things */
    fclose(pFile);
    close (sock_client);
-}
+} 
+
+
+
+
